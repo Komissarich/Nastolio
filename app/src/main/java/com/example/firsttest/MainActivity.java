f3c12bb767398ae8886cc7ddd8557c0b7ecf968e
Komissarich <115020706+Komissarich@users.noreply.github.com>
2023-03-28 19:07:47 +0400

    Update MainActivity.java

--- app/src/main/java/com/example/firsttest/MainActivity.java
+++ app/src/main/java/com/example/firsttest/MainActivity.java
@@ -1,24 +1,36 @@
-package com.example.firsttest;
-
-import static android.util.Log.i;
-
+package com.example.http;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,46 +40,76 @@ public class MainActivity extends AppCompatActivity {
     private static final int CONNECTION_TIMEOUT = 1000;
     private final static String FILE_NAME = "content.txt";
 
-    public String Find() throws IOException {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadDogImage();
+    }
 
+    private void loadDogImage() {
+
+        // getting a new volley request
+        // queue for making new requests
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
         String query = "cthulhuwars";
         int page = 2;
         int start = (page - 1) * 10 + 1;
-        @SuppressLint("DefaultLocale") String url_ = String.format("https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&searchType=image&q=%s&start=%d", Api, Engine, query, start);
-        final URL url = new URL(url_);
-        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setConnectTimeout(CONNECTION_TIMEOUT);
-        con.setReadTimeout(CONNECTION_TIMEOUT);
-        Log.d("2222", "DONE");
-        Log.d("2222", String.valueOf(con.getInputStream()));
-        try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-            String inputLine;
-            final StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-                content.append(System.lineSeparator());
+        String url_ = String.format("https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&searchType=image&q=%s&start=%d", Api, Engine, query, start);
+        URL url = null;
+        Log.d("EEEEE", url_);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(url_, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        if (null != response) {
+                            String filename = "file.txt";
+                            String outputString = "Hello world!";
+
+                            try {
+                                FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                                outputStream.write(outputString.getBytes());
+                                outputStream.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                            try {
+                                FileInputStream inputStream = openFileInput(filename);
+                                BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+                                StringBuilder total = new StringBuilder();
+                                String line;
+                                while ((line = r.readLine()) != null) {
+                                    total.append(line);
+                                }
+                                r.close();
+                                inputStream.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            Log.i("GOGOGOGo", response.toString());
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
             }
-            System.out.println(content.getClass());
-            return content.toString();
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-            return "";
-        }
-
+        });
+        queue.add(request);
     }
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        test a = new test();
-        setContentView(R.layout.activity_main);
+    private void writeToFile(String data,Context context) {
         try {
-             Log.d("111111", Find());
-        } catch (IOException e) {
-            e.printStackTrace();
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
         }
-
-
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
